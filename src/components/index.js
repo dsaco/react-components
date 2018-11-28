5707013f8a6907278e4ab5e7841ab2bef26d6346
dong <2363140194@qq.com>
2018-11-28 18:30:03 +0800

    1.3.16

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Img } from './Img';
 export { default as Message } from './Message';
 export { default as Button } from './Button';
 export { default as Progress } from './Progress';
+export { default as Pagination } from './Pagination';
