35b7f7c2bb197016360c193bb15e0b629b06490a
dsaco <2363140194@qq.com>
2019-04-30 13:58:03 +0800

    update message

--- src/components/Message/index.jsx
+++ src/components/Message/index.jsx
@@ -1,46 +1,35 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-
-import { Transition } from 'react-spring';
+import { Transition } from 'react-spring/renderprops';
 
 const now = Date.now();
 let key = 1;
 
-class Notice extends PureComponent {
-    static defaultProps = {
-        duration: 2,
-    }
-    componentDidMount() {
-        this.startCloseTimer();
-    }
-    close = () => {
-        this.props.onClose();
-    }
-    startCloseTimer = () => {
-        const { duration } = this.props;
-        console.log(duration)
-        this.closeTimer = setTimeout(this.close, duration * 1000);
-    }
-    render() {
-        const { type, ...other } = this.props;
-        return (
-            <div className="ds-message-item" {...other}>
-                {
-                    type === 'success' && <svg style={{marginRight: 10}} viewBox="64 64 896 896" data-icon="check-circle" width="1em" height="1em" fill="#74c140" aria-hidden="true" focusable="false"><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm193.5 301.7l-210.6 292a31.8 31.8 0 0 1-51.7 0L318.5 484.9c-3.8-5.3 0-12.7 6.5-12.7h46.9c10.2 0 19.9 4.9 25.9 13.3l71.2 98.8 157.2-218c6-8.3 15.6-13.3 25.9-13.3H699c6.5 0 10.3 7.4 6.5 12.7z"></path></svg>
-                }
-                {
-                    type === 'error' && <svg style={{marginRight: 10}} viewBox="64 64 896 896" data-icon="close-circle" width="1em" height="1em" fill="#df3c38" aria-hidden="true" focusable="false"><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm165.4 618.2l-66-.3L512 563.4l-99.3 118.4-66.1.3c-4.4 0-8-3.5-8-8 0-1.9.7-3.7 1.9-5.2l130.1-155L340.5 359a8.32 8.32 0 0 1-1.9-5.2c0-4.4 3.6-8 8-8l66.1.3L512 464.6l99.3-118.4 66-.3c4.4 0 8 3.5 8 8 0 1.9-.7 3.7-1.9 5.2L553.5 514l130 155c1.2 1.5 1.9 3.3 1.9 5.2 0 4.4-3.6 8-8 8z"></path></svg>
-                }
-                {
-                    type === 'warn' && <svg style={{marginRight: 10}} viewBox="64 64 896 896" data-icon="exclamation-circle" width="1em" height="1em" fill="#faad14" aria-hidden="true" focusable="false"><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm-32 232c0-4.4 3.6-8 8-8h48c4.4 0 8 3.6 8 8v272c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8V296zm32 440a48.01 48.01 0 0 1 0-96 48.01 48.01 0 0 1 0 96z"></path></svg>
-                }
-                {
-                    type === 'info' && <svg style={{marginRight: 10}} viewBox="64 64 896 896" data-icon="info-circle" width="1em" height="1em" fill="#1890ff" aria-hidden="true" focusable="false"><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm32 664c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8V456c0-4.4 3.6-8 8-8h48c4.4 0 8 3.6 8 8v272zm-32-344a48.01 48.01 0 0 1 0-96 48.01 48.01 0 0 1 0 96z"></path></svg>
-                }
-                <span>{this.props.children}</span>
-            </div>
-        )
-    }
+const Notice = ({duration, type, onClose, children, style}) => {
+    duration = duration || 2;
+    useEffect(() => {
+        const timer = setTimeout(onClose, duration * 1000);
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [])
+    return (
+        <div className={`ds-message-item ${type}`} style={style}>
+            {
+                type === 'success' && <svg style={{marginRight: 10}} viewBox="64 64 896 896" data-icon="check-circle" width="1em" height="1em" fill="#74c140" aria-hidden="true" focusable="false"><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm193.5 301.7l-210.6 292a31.8 31.8 0 0 1-51.7 0L318.5 484.9c-3.8-5.3 0-12.7 6.5-12.7h46.9c10.2 0 19.9 4.9 25.9 13.3l71.2 98.8 157.2-218c6-8.3 15.6-13.3 25.9-13.3H699c6.5 0 10.3 7.4 6.5 12.7z"></path></svg>
+            }
+            {
+                type === 'error' && <svg style={{marginRight: 10}} viewBox="64 64 896 896" data-icon="close-circle" width="1em" height="1em" fill="#df3c38" aria-hidden="true" focusable="false"><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm165.4 618.2l-66-.3L512 563.4l-99.3 118.4-66.1.3c-4.4 0-8-3.5-8-8 0-1.9.7-3.7 1.9-5.2l130.1-155L340.5 359a8.32 8.32 0 0 1-1.9-5.2c0-4.4 3.6-8 8-8l66.1.3L512 464.6l99.3-118.4 66-.3c4.4 0 8 3.5 8 8 0 1.9-.7 3.7-1.9 5.2L553.5 514l130 155c1.2 1.5 1.9 3.3 1.9 5.2 0 4.4-3.6 8-8 8z"></path></svg>
+            }
+            {
+                type === 'warn' && <svg style={{marginRight: 10}} viewBox="64 64 896 896" data-icon="exclamation-circle" width="1em" height="1em" fill="#faad14" aria-hidden="true" focusable="false"><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm-32 232c0-4.4 3.6-8 8-8h48c4.4 0 8 3.6 8 8v272c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8V296zm32 440a48.01 48.01 0 0 1 0-96 48.01 48.01 0 0 1 0 96z"></path></svg>
+            }
+            {
+                type === 'info' && <svg style={{marginRight: 10}} viewBox="64 64 896 896" data-icon="info-circle" width="1em" height="1em" fill="#1890ff" aria-hidden="true" focusable="false"><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm32 664c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8V456c0-4.4 3.6-8 8-8h48c4.4 0 8 3.6 8 8v272zm-32-344a48.01 48.01 0 0 1 0-96 48.01 48.01 0 0 1 0 96z"></path></svg>
+            }
+            <span>{children}</span>
+        </div>
+    )
 }
 
 export class Notification extends PureComponent {
@@ -110,8 +99,5 @@ export default class Message {
     }
     static success = (msg, duration) => {
         getInst().add(msg, 'success', duration);
-    }
-    static config = (options = {}) => {
-        Object.assign(Notice.defaultProps, options);
     }
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,39 +4,34 @@ import Message from '../../components/Message';
 
 export default class extends Component {
 
-    add = () => {
+    open = () => {
         Message.open('普通信息', 3)
     }
     info = () => {
-        Message.info('修改成功', 4)
+        Message.info('修改成功', 3)
     }
     success = () => {
-        Message.success('修改成功', 5)
+        Message.success('修改成功', 2)
     }
     warn = () => {
-        Message.warn('内部错误', 6)
+        Message.warn('内部错误', 3)
     }
     error = () => {
-        Message.error('内部错误 5s', 5)
+        Message.error('内部错误 5s', 2)
     }
     dark = () => {
-        Message.dark('黑暗系列', 10)
-    }
-    turn = () => {
-        Message.config({
-            duration: 10,
-        })
+        Message.dark('黑暗系列', 3)
     }
     render() {
         return (
             <div>
-                <button onClick={this.add}>add</button>
+                <h3>Message</h3>
+                <button onClick={this.open}>open</button>
                 <button onClick={this.success}>success</button>
                 <button onClick={this.info}>info</button>
                 <button onClick={this.warn}>warn</button>
                 <button onClick={this.error}>error</button>
                 <button onClick={this.dark}>dark</button>
-                <button onClick={this.turn}>config 5s</button>
             </div>
         )
     }
