691ab402e2935d68807e2189b24891e8bb3f81cc
dsaco <2363140194@qq.com>
2019-11-15 18:35:30 +0800

    Ripple

---
+++ src/components/Ripple/index.jsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import { Transition } from 'react-spring/renderprops';
+
+let key = 1;
+export default class Ripple extends React.PureComponent {
+    static defaultProps = {
+        rippleColor: 'rgba(0, 0, 0, 0.3)',
+    }
+    state = {
+        top: 0,
+        left: 0,
+        width: 0,
+        height: 0,
+        ripples: [],
+    }
+    lastTouchTime = 0
+    onMouseDown = (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        this.lastTouchTime = Date.now();
+        const { x, y, width, height } = e.target.getBoundingClientRect();
+        const _x = e.clientX || e.pageX;
+        const _y = e.clientY || e.pageY;
+        const w = Math.abs(_x - x);
+        const h = Math.abs(_y - y);
+        const max_w = Math.max(width - w, w);
+        const max_h = Math.max(height - h, h);
+        const r = Math.sqrt(max_w ** 2 + max_h ** 2);
+        const ripple = {
+            key: `ripple-${Date.now()}-${++key}`,
+            top: _y - y - r,
+            left: _x - x - r,
+            width: r * 2,
+            height: r * 2,
+        };
+        const { ripples } = this.state;
+        const _ripples = [...ripples];
+        _ripples.push(ripple);
+        this.setState({ripples: _ripples});
+    }
+    remove = () => {
+        const now = Date.now();
+        const cha = now - this.lastTouchTime;
+        if (cha > 300) {
+            this.setState({ripples: []});
+        } else {
+            setTimeout(() => {
+                const { ripples } = this.state;
+                const _ripples = [...ripples];
+                _ripples.shift();
+                this.setState({ripples: _ripples});
+            }, 350 - cha);
+        }
+    }
+    render() {
+        const { ripples } = this.state;
+        const { rippleColor } = this.props;
+        return (
+            <div style={{position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', overflow: 'hidden'}}
+                onMouseDown={this.onMouseDown} 
+                onMouseLeave={this.remove} 
+                onMouseUp={this.remove}
+            >
+                <Transition
+                    items={ripples}
+                    keys={(item) => item.key}
+                    from={{transform: `scale(0)`, opacity: 1}}
+                    enter={{transform: `scale(1)`}}
+                    leave={{opacity: 0}}
+                    config={{duration: 300}}
+                >
+                    {
+                        (item) => (props) => {
+                            const { key, ...rest } = item;
+                            return (
+                                <div style={{...rest, ...props, backgroundColor: rippleColor, position: 'absolute', pointerEvents: 'none', borderRadius: '50%' }} />
+                            );
+                        }
+                    }
+                </Transition>
+            </div>
+        );
+    }
+}
--- src/pages/Buttons/index.jsx
+++ src/pages/Buttons/index.jsx
@@ -23,6 +23,18 @@ export default class extends Component {
                 <Button disabled={true} variant="text" color="secondary" style={{marginRight: 10}} onClick={() => console.log('disabled')}>disabled</Button>
                 <hr />
                 <Button style={{width: 50, height: 50, borderRadius: '50%'}} color="primary" onClick={() => console.log('secondary')}>+</Button>
+                <hr />
+                <button className="ds-button" onClick={this.dark}>dark</button>
+                <button className="ds-button primary" onClick={this.dark}>dark</button>
+                <button className="ds-button secondary" onClick={this.dark}>dark</button>
+                <hr />
+                <button className="ds-button outline" onClick={this.dark}>dark</button>
+                <button className="ds-button outline primary" onClick={this.dark}>dark</button>
+                <button className="ds-button outline secondary" onClick={this.dark}>dark</button>
+                <hr />
+                <button className="ds-button text" onClick={this.dark}>dark</button>
+                <button className="ds-button text primary" onClick={this.dark}>dark</button>
+                <button className="ds-button text secondary" onClick={this.dark}>dark</button>
             </div>
         )
     }
